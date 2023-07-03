7821913135752f3e0398b4d4bf742e0711c8ef4d
Sebastian Döll <sebastian@katallaxie.me>
2023-07-03 18:47:49 +0000

    chore: add config test

---
+++ pkg/config/config_test.go
@@ -0,0 +1,33 @@
+package config_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/katallaxie/csync/pkg/config"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+	t.Parallel()
+
+	c := config.New()
+
+	assert.NotNil(t, c)
+	assert.Equal(t, c.File, ".csync.yml")
+	assert.NotNil(t, c.Stderr)
+	assert.NotNil(t, c.Stdout)
+	assert.NotNil(t, c.Stdout)
+	assert.Equal(t, c.Stderr, os.Stderr)
+	assert.Equal(t, c.Stdout, os.Stdout)
+	assert.Equal(t, c.Stderr, os.Stderr)
+
+	h, err := c.HomeDir()
+	assert.NoError(t, err)
+	assert.NotEmpty(t, h)
+
+	cwd, err := c.Cwd()
+	assert.NoError(t, err)
+	assert.NotEmpty(t, cwd)
+}
