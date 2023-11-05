ea1671e58090c6563dbf376ec32d31fcf06f0d69
Sebastian Döll <sebastian@katallaxie.me>
2023-11-05 12:09:42 +0000

    chore: add macOS app to List() function

--- internal/spec/apps.go
+++ internal/spec/apps.go
@@ -17,5 +17,6 @@ func List() []App {
 		Homebrew(),
 		Hyper(),
 		Kubectl(),
+		MacOS(),
 	}
 }
---
+++ internal/spec/macos.go
@@ -0,0 +1,17 @@
+package spec
+
+// MacOS is the configuration for the macOS.
+func MacOS() App {
+	return App{
+		Name: "macOS",
+		Files: Files{
+			"Library/Preferences/com.apple.Terminal.plist",
+			"Library/PDF Services",
+			"Library/Preferences/com.apple.symbolichotkeys.plist",
+			"Library/Scripts",
+			"Library/Speech/Speakable Items",
+			"Library/Workflows",
+			".MacOSX",
+		},
+	}
+}
