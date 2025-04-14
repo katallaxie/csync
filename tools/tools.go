e5e18b9e20a00a1eee6f95879eb1c7a586630192
Sebastian Döll <sebastian@katallaxie.dev>
2025-04-14 08:23:41 +0000

    chore: update tools

---
+++ tools/tools.go
@@ -0,0 +1,12 @@
+//go:build tools
+// +build tools
+
+package tools
+
+import (
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/goreleaser/goreleaser"
+	_ "github.com/vektra/mockery/v2"
+	_ "gotest.tools/gotestsum"
+	_ "mvdan.cc/gofumpt"
+)
