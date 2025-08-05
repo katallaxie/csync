499272014cd4c462489d88023c987a3a488de484
Sebastian Döll <sebastian@katallaxie.dev>
2025-08-05 19:59:50 +0000

    chore: update protoc

--- scripts/postCreateCommand.sh
+++ scripts/postCreateCommand.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # This script is executed after the creation of a new project.
 
-echo 'deb [trusted=yes] https://repo.goreleaser.com/apt/ /' | sudo tee /etc/apt/sources.list.d/goreleaser.list
-sudo apt update
-sudo apt install -y goreleaser direnv protobuf-compiler
 go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
 go install github.com/yoheimuta/protolint/cmd/protolint@latest
